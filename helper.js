a542c9c230715f6c69d2cce2b6012b4dd23e4430
zhangzhenyang <zhangzhenyang@baidu.com>
2017-03-16 15:19:05 +0800

    update dist files

--- helper.js
+++ helper.js
@@ -3,4 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./helper"));
+__export(require("./dist/helper"));
--- react.js
+++ react.js
@@ -3,4 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./react"));
+__export(require("./dist/react"));
