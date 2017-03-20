9f10383523932feb8a9d56d1f839799a778c50b5
zhangzhenyang <zhangzhenyang@baidu.com>
2017-03-20 18:10:53 +0800

    update entry

--- test.js
+++
@@ -1,9 +0,0 @@
-const ioc = require('./dist')
-const utils = require('./dist/utils')
-const context = ioc.IocContext.DefaultInstance
-
-class AClass { }
-class BClass extends AClass { }
-class CClass extends BClass { }
-
-context.register(CClass, undefined, { regInSuperClass: true })
