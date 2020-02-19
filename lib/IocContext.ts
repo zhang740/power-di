9a8631f5201c159c6bb45ead98557abcd46c6c3c
zhang740 <zhang740@vmzclub.com>
2020-02-19 21:15:54 +0800

    refactor: another way for subclasses inject.

--- lib/IocContext.ts
+++ lib/IocContext.ts
@@ -1,53 +1,44 @@
-import { getGlobalType, getSuperClassInfo, isClass, getInjects } from './utils'
-import { logger } from '../utils'
+import { getGlobalType, isClass } from './utils';
+import { logger } from '../utils';
+import { GetReturnType, RegKeyType, KeyType, ClassType } from './utils/types';
+import { getInjects, getMetadata } from './class/metadata';
+import { classLoader } from './class/ClassLoader';
+import { guard } from './utils/guard';
 
 export class Config {
-  /** auto register class, when class not found. default: false */
-  autoRegister?: boolean = false
+  /** auto register class self, when class not found. default: false */
+  autoRegisterSelf?: boolean = false;
   /** constructor inject, MUST in TypeScript with emitDecoratorMetadata and use decorator with class, default: true */
-  constructorInject?: boolean = true
-  // onNotFound: (globalType: string) => any
-  // onGetValue: (data: Store) => any
+  constructorInject?: boolean = true;
+  /** use class loader for autowired default */
+  useClassLoader?: boolean = true;
+  notFoundHandler?: (type: KeyType) => any;
 }
 
 export const DefaultRegisterOption: RegisterOptions = {
   singleton: true,
   autoNew: true,
-  regInSuperClass: false,
-}
+};
+
 export interface RegisterOptions {
   /** default: true */
-  singleton?: boolean
+  singleton?: boolean;
   /** if data a class, auto new a instance.
    *  if data a function, auto run(lazy).
    *  default: true */
-  autoNew?: boolean
-  /**
-   * register in superclass, you can get use superclass with getSubClasses method.
-   * This setting is not inherited.
-   * default: false
-   * */
-  regInSuperClass?: boolean
+  autoNew?: boolean;
 }
-export type ClassType = new (...args: any[]) => any
-export type InterceptorType = () => void
-export type KeyType = Function | string // typeof Object
-export type RegKeyType = KeyType | undefined
-export type GetReturnType<T, ClsType> = T extends undefined ?
-  (ClsType extends new (...args: any[]) => infer R ? R : any)
-  : T
 export interface Store {
-  inited: boolean,
-  value: any,
-  options: RegisterOptions,
-  subClasses: Store[],
+  inited: boolean;
+  value: any;
+  options: RegisterOptions;
 }
 export class IocContext {
-  private static defaultInstance: IocContext
-  private components = new Map<string, Store>()
+  private static defaultInstance: IocContext;
+  private components = new Map<string, Store>();
   public static get DefaultInstance() {
     return this.defaultInstance ||
-      (this.defaultInstance = new IocContext(), this.defaultInstance)
+      (this.defaultInstance = new IocContext(), this.defaultInstance);
   }
 
   constructor(
@@ -56,192 +47,215 @@ export class IocContext {
   }
 
   public remove(keyOrType: KeyType) {
-    return this.components.delete(getGlobalType(keyOrType))
+    return this.components.delete(getGlobalType(keyOrType));
   }
 
   public clear() {
-    this.components.clear()
+    this.components.clear();
   }
 
   public get<T = undefined, KeyOrType = any>(keyOrType: KeyOrType): GetReturnType<T, KeyOrType> {
-    const key = getGlobalType(keyOrType)
+    const key = getGlobalType(keyOrType);
 
     if (this.components.has(key)) {
-      return this.returnValue(this.components.get(key))
-    } else if (this.config.autoRegister && isClass(keyOrType)) {
-      this.register(keyOrType)
-      return this.get(keyOrType)
+      return this.returnValue(this.components.get(key));
+    }
+
+    if (this.config.notFoundHandler) {
+      const data = this.config.notFoundHandler(keyOrType as any);
+      if (data) {
+        return data;
+      }
+    }
+
+    if (this.config.useClassLoader) {
+      const type = keyOrType as any;
+      const classes = classLoader.getImplementClasses(type);
+      switch (classes.length) {
+        case 1:
+          this.register(classes[0], type);
+          return this.get(type);
+
+        case 0:
+          break;
+
+        default:
+          throw new MultiImplementError(type, key);
+      }
+    }
+
+    const canAutoRegister = isClass(keyOrType) && (
+      this.config.autoRegisterSelf ||
+      getMetadata(keyOrType as any).injectable
+    );
+    if (canAutoRegister) {
+      this.register(keyOrType);
+      return this.get(keyOrType);
+    }
+
+    throw new NotfoundTypeError(keyOrType, key);
+  }
+
+  public getImports<T = undefined, KeyOrType = any>(keyOrType: KeyOrType, { cache }: {
+    cache?: boolean;
+  } = {}): GetReturnType<T, KeyOrType>[] {
+    const key = getGlobalType(keyOrType);
+    if (this.has(key)) {
+      return this.get(key);
     } else {
-      return
+      const type = keyOrType as any;
+      const data = classLoader.getImplementClasses(type).map(cls => {
+        if (this.has(cls)) {
+          return this.get(cls);
+        }
+        return this.genValueFactory(cls)();
+      });
+      if (cache) {
+        this.register(data, key);
+      }
+      return data;
     }
   }
 
   public has(keyOrType: KeyType): boolean {
-    return this.components.has(getGlobalType(keyOrType))
-  }
-
-  public getSubClasses<T>(keyOrType: KeyType): T[] {
-    const data = this.components.get(getGlobalType(keyOrType))
-    if (!data) return
-    return data.subClasses.map(sc => this.returnValue(sc))
+    return this.components.has(getGlobalType(keyOrType));
   }
 
   public replace(keyOrType: KeyType, newData: any, options?: RegisterOptions, force = false) {
-    const key = getGlobalType(keyOrType)
-    const data = this.components.get(key)
+    const key = getGlobalType(keyOrType);
+    const data = this.components.get(key);
     if (data) {
-      data.inited = false
-      data.value = this.genValue(newData, options || data.options)
+      data.inited = false;
+      data.value = this.genValueFactory(newData, options || data.options);
     } else if (force) {
-      this.register(newData, keyOrType, options)
+      this.register(newData, keyOrType, options);
     } else {
-      throw new Error(`the key:[${key}] is not register.`)
+      throw new Error(`the key:[${key}] is not register.`);
     }
-  }
-
-  public append(keyOrType: KeyType, subData: any, options = DefaultRegisterOption) {
-    if (!this.canBeKey(keyOrType)) {
-      throw new Error('key require a string or a class.')
-    }
-    let store: Store
-    if (isClass(subData)) {
-      this.register(subData, undefined, options)
-      store = this.components.get(getGlobalType(subData))
-    } else {
-      store = this.newStore(subData, options)
-    }
-    this.appendData(getGlobalType(keyOrType), keyOrType, options, store)
   }
 
   public register(data: any, key?: RegKeyType, options = DefaultRegisterOption) {
     if (key) {
       if (!this.canBeKey(key)) {
-        throw new Error('key require a string or a class.')
+        throw new Error('key require a string or a class.');
       }
     } else {
       if (!this.canBeKey(data)) {
-        throw new Error('when data is not a class or string, require a key.')
+        throw new Error('when data is not a class or string, require a key.');
       }
     }
-    const dataType = (key && getGlobalType(key)) || (data && getGlobalType(data))
+    const dataType = (key && getGlobalType(key)) || (data && getGlobalType(data));
 
     if (this.components.has(dataType)) {
-      throw new Error(`the key:[${dataType}] is already register.`)
+      throw new Error(`the key:[${dataType}] is already register.`);
     }
     options = {
       ...DefaultRegisterOption,
       ...options,
-    }
-    const store: Store = this.newStore(data, options)
-    this.components.set(dataType, store)
-
-    if (options.regInSuperClass) {
-      if (!(data instanceof Function)) {
-        throw new Error('if need regInSuperClass, data MUST be a class.')
-      }
-      const newOptions: RegisterOptions = { ...options, regInSuperClass: false }
-      const superClasses = getSuperClassInfo(data)
-      superClasses.forEach(sc => this.appendData(sc.type, sc.class, newOptions, store))
-    }
+    };
+    const store: Store = this.newStore(data, options);
+    this.components.set(dataType, store);
   }
 
-  public inject(instance: any, notFoundHandler?: (globalType: string, typeCls?: any) => any) {
-    const iocSelf = this
-    const classType = instance.constructor.prototype
+  public inject(instance: any) {
+    const iocSelf = this;
+    const classType = instance.constructor;
     getInjects(classType)
       .forEach(inject => {
-        const { key, typeCls, globalType } = inject
-        switch (inject.type) {
-          case 'inject':
-            let data = this.get(typeCls)
-            if (data === undefined) {
-              if (notFoundHandler) {
-                data = notFoundHandler(globalType, typeCls)
-              }
-              if (data === undefined) {
-                logger.warn('Notfound:' + globalType)
-              }
-            }
-            Object.defineProperty(instance, key, {
-              configurable: true,
-              writable: true,
-              value: data,
-            })
-            break
+        const { key, typeCls, optional } = inject;
 
-          case 'lazyInject':
-            const { always, subClass } = inject
+        const descriptor = Object.getOwnPropertyDescriptor(instance, key);
+        let defaultValue: any = descriptor && descriptor.value;
+        const allowOptional = optional || defaultValue !== undefined;
 
-            const descriptor = Object.getOwnPropertyDescriptor(instance, key)
-            let defaultValue: any = descriptor && descriptor.value
-            Object.defineProperty(instance, key, {
-              configurable: true,
-              get: function () {
-                let data = subClass ? iocSelf.getSubClasses(typeCls) : iocSelf.get(typeCls)
-                if (data === undefined) {
-                  if (notFoundHandler) {
-                    data = notFoundHandler(globalType, typeCls)
-                  }
-                  if (data === undefined) {
-                    logger.warn(`Notfound: ${globalType}, use defaultValue.`)
-                  }
-                } else {
-                  defaultValue = undefined
-                  if (!always) {
-                    Object.defineProperty(this, key, {
-                      configurable: true,
-                      writable: true,
-                      value: data
-                    })
+        if ('inject' === inject.type) {
+          Object.defineProperty(instance, key, {
+            configurable: true,
+            writable: true,
+            value: guard(() => this.get(typeCls), {
+              defaultValue,
+              onError: (err) => {
+                if (!allowOptional) {
+                  throw err;
+                }
+              }
+            }),
+          });
+        }
+
+        if (['lazyInject', 'imports'].includes(inject.type)) {
+          const { always } = inject;
+          Object.defineProperty(instance, key, {
+            configurable: true,
+            get: function () {
+              let hasErr = false;
+              const data = guard(() => {
+                switch (inject.type) {
+                  case 'lazyInject':
+                    return iocSelf.get(typeCls);
+                  case 'imports':
+                    return iocSelf.getImports(typeCls, { cache: !always });
+                }
+              }, {
+                defaultValue,
+                onError: (err) => {
+                  hasErr = true;
+                  if (!allowOptional) {
+                    throw err;
                   }
                 }
-                return data || defaultValue
-              },
-              set: function (value) {
+              });
+              if (!hasErr && !always) {
                 Object.defineProperty(this, key, {
                   configurable: true,
                   writable: true,
-                  value,
-                })
+                  value: data
+                });
               }
-            })
-            break
+              return data;
+            },
+            set: function (value) {
+              Object.defineProperty(this, key, {
+                configurable: true,
+                writable: true,
+                value,
+              });
+            }
+          });
         }
-      })
+      });
   }
 
-  private appendData(keyType: string, typeData: any, options: RegisterOptions, store: Store) {
-    let superClass = this.components.get(keyType)
-    if (!superClass) {
-      this.register(typeData, undefined, options)
-      superClass = this.components.get(keyType)
-    }
-    superClass.subClasses.push(store)
+  public createChildContext(config = this.config) {
+    return new IocContext({
+      ...config,
+      notFoundHandler: type => this.get(type)
+    });
   }
 
   private newStore(data: any, options: RegisterOptions) {
     return {
       inited: false,
-      value: this.genValue(data, options),
+      value: this.genValueFactory(data, options),
       options,
       subClasses: []
-    } as Store
+    } as Store;
   }
 
   private canBeKey(obj: any) {
-    return obj instanceof Function || typeof obj === 'string'
+    return obj instanceof Function || ['string', 'symbol'].includes(typeof obj);
   }
 
-  private genValue(data: any, options: RegisterOptions) {
-    const dataIsFunction = data instanceof Function
-    const dataIsClass = dataIsFunction && isClass(data)
+  private genValueFactory(data: any, options: RegisterOptions = DefaultRegisterOption) {
+    const dataIsFunction = data instanceof Function;
+    const dataIsClass = dataIsFunction && isClass(data);
 
     return () => {
       if (dataIsFunction && options.autoNew) {
         if (dataIsClass) {
-          let args: any[] = [this]
+          let args: any[] = [this];
           if (this.config.constructorInject && Reflect && Reflect.getMetadata) {
-            const paramTypes = Reflect.getMetadata('design:paramtypes', data)
+            const paramTypes = Reflect.getMetadata('design:paramtypes', data);
             if (paramTypes) {
               args = paramTypes.map((type: any) => {
                 if (type === data ||
@@ -255,22 +269,22 @@ export class IocContext {
                   type === Array ||
                   type === Function
                 ) {
-                  return null
+                  return null;
                 }
-                return this.get(type)
-              })
+                return this.get(type);
+              });
             }
           }
-          const value = new data(...args)
-          this.inject(value)
-          return value
+          const value = new data(...args);
+          this.inject(value);
+          return value;
         } else {
-          return data(this)
+          return data(this);
         }
       } else {
-        return data
+        return data;
       }
-    }
+    };
   }
 
   private returnValue(data: Store) {
@@ -280,9 +294,21 @@ export class IocContext {
           data.inited = true,
           data.value = data.value(),
           data.value
-        )
+        );
     } else {
-      return data.value()
+      return data.value();
     }
+  }
+}
+
+export class MultiImplementError extends Error {
+  constructor(type: ClassType, key: string) {
+    super(`Has multi Classes of implement type: ${type.name}(${key})`);
+  }
+}
+
+export class NotfoundTypeError extends Error {
+  constructor(type: any, key: string) {
+    super(`Notfound type: ${type.name || type}(${key})`);
   }
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,3 @@
-export * from './IocContext'
+export * from './IocContext';
+export * from './class';
+export * from './decorator';
