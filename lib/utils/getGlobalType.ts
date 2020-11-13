64e2c95a6d7f69e6cfddd60f96baffa66eeb5bcc
zhang740 <zhang740@vmzclub.com>
2020-11-13 19:40:56 +0800

    update: type guard for isClass.

--- lib/utils/getGlobalType.ts
+++ lib/utils/getGlobalType.ts
@@ -4,10 +4,11 @@ let _uid = 0;
 
 const _globalTypes: { [key: string]: boolean } = {};
 
-export function isClass(target: any) {
+export function isClass(target: any): target is Function {
   return target instanceof Function
     && (target.toString().match(/\w+/g)[0] === 'class' || target.toString().match(/\w+/g)[0] === 'function');
   // If browser, maybe no class
+  // getPrototypeOf a class, is not Function, not instanceof Function, but typeof 'function'
 }
 
 /**
