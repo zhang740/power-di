158a5f2cce38081935cb99cb57bfe2d68b11933f
paranoidjk <hust2012jiangkai@gmail.com>
2021-07-08 15:31:36 +0800

    feat: pureComponent support config (#6)

--- lib/react/IocConsumer.tsx
+++ lib/react/IocConsumer.tsx
@@ -5,15 +5,17 @@ import { createConsumerComponent } from './BaseComponent';
 export interface IocConsumerOpt {
   /** manual extends BaseClass: BaseConsumerComponent */
   manualExtendsBaseClass?: boolean;
+  /** should be pure component */
+  pureComponent?: boolean;
 }
 
 export function iocConsumer(opt: IocConsumerOpt = {}): ClassDecorator {
   opt = Object.assign({ useBaseClass: false }, opt);
 
-  return function (Comp: any): void {
+  return function (Comp: any): any {
     const NewComp: any = opt.manualExtendsBaseClass ? Comp : createConsumerComponent(Comp);
 
-    class IoCComponent extends React.PureComponent {
+    class IoCPureComponent extends React.PureComponent {
       static displayName = `IoCComponent(${getDisplayName(Comp)})`;
 
       render() {
@@ -31,7 +33,25 @@ export function iocConsumer(opt: IocConsumerOpt = {}): ClassDecorator {
       }
     }
 
-    return IoCComponent as any;
+    class IoCComponent extends React.Component {
+      static displayName = `IoCComponent(${getDisplayName(Comp)})`;
+
+      render() {
+        return (
+          <Context.Consumer>
+            {ctx => (
+              <NewComp
+                {...Object.assign({}, this.props, {
+                  [ContextSymbol]: ctx,
+                })}
+              />
+            )}
+          </Context.Consumer>
+        );
+      }
+    }
+
+    return opt.pureComponent ? IoCPureComponent : IoCComponent;
   };
 }
 
