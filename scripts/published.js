8b2c36c71ca560fabe54db4d308debf4850cf60e
zhang740 <zhang740@vmzclub.com>
2021-08-17 17:04:08 +0800

    fix: build

--- scripts/published.js
+++
@@ -1,6 +0,0 @@
-const shell = require('child_process').execSync;
-const package = require('../package.json');
-shell(`git tag ${package.version}`);
-shell(`git push`);
-shell(`git push --tags`);
-//# sourceMappingURL=published.js.map
