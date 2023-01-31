4202addc816408add2594a7a3e31d16570d44dd5
zhang740 <zhang740@vmzclub.com>
2023-01-31 15:25:20 +0800

    fix: Notfound tips

--- lib/react/IocProvider.tsx
+++ lib/react/IocProvider.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Context } from './context';
 import { IocContext } from '../IocContext';
 
-export const IocProvider: React.FC<{ context?: IocContext }> = props => {
+export const IocProvider: React.FC<{
+  context?: IocContext;
+  children?: React.ReactNode | undefined;
+}> = props => {
   return (
     <Context.Provider value={props.context || IocContext.DefaultInstance}>
       {props.children}
