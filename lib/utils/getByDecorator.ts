85106660d20da7834fdfd9f6b55ccb8e467e311a
zhangyuan <zhangyuan.8503@bytedance.com>
2024-06-12 23:06:26 +0800

    feat: has support class loader.

--- lib/utils/getByDecorator.ts
+++ lib/utils/getByDecorator.ts
@@ -1,9 +1,26 @@
-import 'reflect-metadata';
 import { getGlobalType } from './getGlobalType';
 
+declare global {
+  namespace Reflect {
+    function getMetadata(metadataKey: any, target: Object): any;
+    function getMetadata(metadataKey: any, target: Object, propertyKey: string | symbol): any;
+  }
+}
+
+try {
+  require('reflect-metadata');
+} catch (error) {}
+
+export function getReflectMetadata(metadataKey: any, target: Object, key?: string | symbol) {
+  /* istanbul ignore else */
+  if (Reflect && Reflect.getMetadata) {
+    return Reflect.getMetadata(metadataKey, target, key);
+  }
+}
+
 export function getClsTypeByDecorator(type: any, target: any, key: string | symbol) {
-  if (!type && Reflect && Reflect.getMetadata) {
-    type = Reflect.getMetadata('design:type', target, key);
+  if (type === undefined) {
+    type = getReflectMetadata('design:type', target, key);
   }
   return type;
 }
